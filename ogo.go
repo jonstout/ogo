f8eedc3bf064a36496f9ce9519379012489f4d1c
Jonathan Stout <jonstout@indiana.edu>
2013-04-28 11:55:21 -0900

    adding logic for learning switch

--- ogo.go
+++ ogo.go
@@ -11,11 +11,13 @@ import (
 type DemoApplication struct {
 	ogo.OgoApplication
 	packetIn chan ofp10.OfpMsg
+	hostMap map[string]int
 }
 
 func (b *DemoApplication) InitApplication(args map[string]string) {
 	b.Name = "Demo"
 	b.packetIn = ogo.SubscribeTo(ofp10.OFPT_PACKET_IN)
+	b.hostMap = make(map[string]int)
 }
 
 func (b *DemoApplication) Receive() {
@@ -31,11 +33,23 @@ func (b *DemoApplication) Receive() {
 
 func (b *DemoApplication) parsePacketIn(dpid string, pkt *ofp10.OfpPacketIn) {
 	if s, ok := ogo.GetSwitch(dpid); ok {
-		pktOut := ofp10.NewPacketOut()
-		pktOut.Actions = append(pktOut.Actions, ofp10.NewActionOutput())
 		if eth, ok := pkt.Data.(*pacit.Ethernet); ok {
-			pktOut.Data = eth
-			s.Send(pktOut)
+			if src, ok := b.hostMap[eth.HWSrc.String()]; ok {
+				if dst, ok := b.hostMap[eth.HWDst.String()]; ok {
+					// Match := src,dst Fwd := dst.port
+					// Match := dst,src Fwd := src.port
+					fmt.Println("Src:", src, "and Dst:", dst, "known.")
+				} else {
+					// Flood
+					pktOut := ofp10.NewPacketOut()
+					pktOut.Actions = append(pktOut.Actions, ofp10.NewActionOutput())
+					pktOut.Data = pkt.Data
+					s.Send(pktOut)
+				}
+			} else {
+				// Add to map
+				b.hostMap[eth.HWSrc.String()] = int(pkt.InPort)
+			}
 		}
 	}
 }
