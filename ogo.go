6b5e149b77de6f30cf779e625daa0917f0886322
ogo <ogo@ubuntu.(none)>
2013-05-09 21:12:49 -0400

    revering per switch mac map

--- ogo.go
+++ ogo.go
@@ -9,14 +9,14 @@ import (
 // This is a basic learning switch implementation
 type DemoApplication struct {
 	packetIn chan ofp10.OfpMsg
-	hostMap map[string]map[string]uint16
+	hostMap map[string]uint16
 }
 
 func (b *DemoApplication) InitApplication(args map[string]string) {
 	// SubscribeTo returns a chan to receive a specific message type.
 	b.packetIn = ogo.SubscribeTo(ofp10.OFPT_PACKET_IN)
 	// A place to store the source ports of MAC Addresses
-	b.hostMap = make(map[string]map[string]uint16)
+	b.hostMap = make(map[string]uint16)
 }
 
 func (b *DemoApplication) Name() string {
@@ -31,9 +31,6 @@ func (b *DemoApplication) Receive() {
 			if pkt, ok := m.Data.(*ofp10.OfpPacketIn); ok {
 				// This could be launched in a separate goroutine,
 				// but maps in Go aren't thread safe.
-				if _, ok := b.hostMap[m.DPID]; !ok {
-					b.hostMap[m.DPID] = make(map[string]uint16)
-				}
 				b.parsePacketIn(m.DPID, pkt)
 			}
 		}
@@ -44,13 +41,13 @@ func (b *DemoApplication) parsePacketIn(dpid string, pkt *ofp10.OfpPacketIn) {
 	eth := pkt.Data
 	hwSrc := eth.HWSrc.String()
 	hwDst := eth.HWDst.String()
-	if _, ok := b.hostMap[dpid][hwSrc]; !ok {
-		b.hostMap[dpid][hwSrc] = pkt.InPort
+	if _, ok := b.hostMap[hwSrc]; !ok {
+		b.hostMap[hwSrc] = pkt.InPort
 	}
-	if _, ok := b.hostMap[dpid][hwDst]; ok {
+	if _, ok := b.hostMap[hwDst]; ok {
 		f1 := ofp10.NewFlowMod()
 		act1 := ofp10.NewActionOutput()
-		act1.Port = b.hostMap[dpid][hwDst]
+		act1.Port = b.hostMap[hwDst]
 		f1.Actions = append(f1.Actions, act1)
 		m1 := ofp10.NewMatch()
 		m1.DLSrc = eth.HWSrc
@@ -60,7 +57,7 @@ func (b *DemoApplication) parsePacketIn(dpid string, pkt *ofp10.OfpPacketIn) {
 		
 		f2 := ofp10.NewFlowMod()
 		act2 := ofp10.NewActionOutput()
-		act2.Port = b.hostMap[dpid][hwSrc]
+		act2.Port = b.hostMap[hwSrc]
 		f2.Actions = append(f1.Actions, act2)
 		m2 := ofp10.NewMatch()
 		m2.DLSrc = eth.HWDst
