3ce6f61250676d89132cd50fa5b604891b511500
Jonathan Stout <jonstout@indiana.edu>
2013-10-15 21:49:49 -0400

    removing comments

--- openflow/ofp.go
+++ openflow/ofp.go
@@ -1,8 +1,3 @@
-// Messages should be created from this package, unless you desire to
-// use a specific version. This package will use the latest supported
-// protocol version to generate messages. Hopefully message types don't
-// change too much in forthcoming OpenFlow specifications. Unsupported
-// message fields will be taken care of by MessageStream.
 package ofp
 
 var SupportedVersions []uint16 = []uint16{1}
