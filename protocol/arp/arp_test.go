0be3ff2644a740756adf1930fcea70f18a1c07c2
Jonathan Stout <jonstout@indiana.edu>
2014-03-09 10:55:40 -0900

    adding arp unmarshal test

--- protocol/arp/arp_test.go
+++ protocol/arp/arp_test.go
@@ -28,5 +28,23 @@ func TestArpMarshalBinary(t *testing.T) {
 }
 
 func TestArpUnmarshalBinary(t *testing.T) {
+	b := "   00 01 " + // HWType
+		"08 00 " + // ProtoType
+		"06 04 " + // HWLength ProtoLength
+		"00 01 " + // Type_Request
+		"00 00 00 00 00 00 " + // HWSrc
+		"00 00 00 00 " + // IPSrc
+		"00 00 00 00 00 00 " + // HWDst
+		"00 00 00 00 "   // IPDst
+	b = strings.Replace(b, " ", "", -1)
+	bytes, _ := hex.DecodeString(b)
 
+	a, _ := New(Type_Reply) // Ensure type is set correctly
+	a.UnmarshalBinary(bytes)
+	
+	if int(a.Len()) != len(bytes) {
+		t.Errorf("Got length of %d, expected %d.", a.Len(), len(bytes))
+	} else if a.Operation != Type_Request {
+		t.Errorf("Got type %d, expected %d.", a.Operation, Type_Request)
+	}
 }
